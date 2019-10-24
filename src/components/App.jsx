82a314dc58e95c90a4ff178214cc5f9b5fb6546d
Aakarsh Chopra <alcatrazz.627@gmail.com>
2019-10-24 11:48:30 +0530

    First Push

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+// import _ from 'lodash'
+
+const App = () => {
+
+    return (
+        <div className="">
+            Henlo stimky
+        </div>
+    )
+}
+
+export default App
